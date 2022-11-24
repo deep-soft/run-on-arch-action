1741966e5d81f1fd4f5d57f49037d2e52151753d
Kingtous <kingtouse@gmail.com>
2022-11-24 08:45:27 +0800

    Update run-on-arch.sh

--- src/run-on-arch.sh
+++ src/run-on-arch.sh
@@ -98,7 +98,7 @@ run_container () {
   docker run \
     --workdir "${GITHUB_WORKSPACE}" \
     --rm \
-    --cpus=1.5 \
+    --memory-swap -1 \
     -e DEBIAN_FRONTEND=noninteractive \
     -e CI \
     -e GITHUB_ACTION \
